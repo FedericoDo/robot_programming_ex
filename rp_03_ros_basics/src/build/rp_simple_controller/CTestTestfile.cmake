8fce10bcbaae890809f41592747ee83ad3e0d92e
FedericoDo <federico.donati.lav@gmail.com>
2025-04-24 12:40:04 +0200

    exercise 3

---
+++ rp_03_ros_basics/src/build/rp_simple_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/federico/Desktop/git/rp_03_ros_basics/src/rp_simple_controller
+# Build directory: /home/federico/Desktop/git/rp_03_ros_basics/src/build/rp_simple_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
